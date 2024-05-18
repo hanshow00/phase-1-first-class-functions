3fe37e5493e5f2440ab85592ad069bb218d6cba8
Hans How <hanshow00@gmail.com>
2024-05-18 15:45:43 -0500

    first class lab

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback) {
+ callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        return "I am a named function";
+    };
+}
+
+const returnsAnAnonymousFunction = function() {
+    return function() {
+        return "I am an anonymous function";
+    };
+}
+
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+  };
